3508ad666d965eabae65833831c8952d2fd087b5
Parth Mittal <mittalparth22@gmail.com>
2023-01-27 13:47:24 +0530

    navbar: Add component

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const navLinks = [
+    {
+      id: "events",
+      title: "Events",
+    },
+    {
+      id: "competitions",
+      title: "Competitions",
+    },
+    {
+      id: "contactUs",
+      title: "Contact Us",
+    },
+  ];
