834efb059e46ae0c66e2d856bfca624d46830b5a
targithu <githubtar@gmail.com>
2023-01-30 10:49:25 +0530

    Res Card

--- src/components/Competitions.jsx
+++ src/components/Competitions.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import CompetitionCard from './CompUtils/CompetitionCard'
 
 const Competitions = () => {
   return (
-    <div>Competitions</div>
+    <section id="competitions">
+      <h1 className='flex-1 font-poppins font-semibold text-[45px] text-primary leading-[80px]'>
+        Competitions
+      </h1>
+
+      <div className='flex flex-wrap sm:flex-row md:flex-row'>
+
+        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
+          <div className='w-full rounded-lg overflow-hidden'>
+            <CompetitionCard/>
+          </div>
+        </div>
+
+        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
+          <div className='w-full rounded-lg overflow-hidden'>
+            <CompetitionCard/>
+          </div>
+        </div>
+
+        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
+          <div className='w-full rounded-lg overflow-hidden'>
+            <CompetitionCard/>
+          </div>
+        </div>
+
+        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
+          <div className='w-full rounded-lg overflow-hidden'>
+            <CompetitionCard/>
+          </div>
+        </div>
+
+        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
+          <div className='w-full rounded-lg overflow-hidden'>
+            <CompetitionCard/>
+          </div>
+        </div>
+
+      </div>
+
+
+    </section>
   )
 }
 
