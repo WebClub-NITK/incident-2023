32849a4fb146b3e27a8b76999182223bbd47c325
targithu <githubtar@gmail.com>
2023-01-30 13:00:59 +0530

    placed coins

--- src/components/CompUtils/CompetitionCard.jsx
+++ src/components/CompUtils/CompetitionCard.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const CompetitionCard = () => {
+const CompetitionCard = (competition) => {
   return (
-    <div className='w-full h-full max-w-sm bg-dimMaroon rounded-lg shadow-lg m-4'>
+    <div className="transition-all duration-100 h-fit w-fit m-0 p-1 rounded-lg bg-dimMaroon m-4 border-black border-2 hover:border-none hover:shadow-inner hover:shadow-black">
+    <div className='w-full h-full min-h-[28rem] max-w-sm bg-dimMaroon rounded-lg'>
         <div className='p-6'>
             {/* hover to some color */}
             {/* text colors and bg are subject to change */}
-        <div className='text-rose-100 hover:text-red-600 cursor-pointer'>
-          <h3 className='text-lg font-medium font-poppins'>Dance</h3>
-        </div>
 
         {/* fix the hover, it should hover only for the head text and not the whole line */}
-
-          <p className='text-sm  font-poppins text-neutral-50 mt-2'>Dance is generally, however not exclusively, performed with the accompaniment of music and may or may not be performed in time to such music. Some dance (such as tap dance) may provide its own audible accompaniment in place of (or in addition to) music. Many early forms of music and dance were created for each other and are frequently performed together. Notable examples of traditional dance/music couplings include the jig, waltz, tango, disco, and salsa. Some musical genres have a parallel dance form such as baroque music and baroque dance; other varieties of dance and music may share nomenclature but developed separately, such as classical music and classical ballet.</p>
+          <div className='w-full'>
+            <p className='text-sm font-poppins text-neutral-50 mt-2 px-3'>{competition.details}</p>
+          </div>
         </div>
+    </div>  
     </div>
   )
 }
--- src/components/Competitions.jsx
+++ src/components/Competitions.jsx
@@ -1,50 +1,31 @@
 import React from 'react'
 import CompetitionCard from './CompUtils/CompetitionCard'
-
+import {compDetails} from '../constants/competition';
 const Competitions = () => {
   return (
     <section id="competitions">
-      <h1 className='flex-1 font-poppins font-semibold text-[45px] text-primary leading-[80px]'>
-        Competitions
-      </h1>
-
       <div className='flex flex-wrap sm:flex-row md:flex-row'>
-
-        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
-          <div className='w-full rounded-lg overflow-hidden'>
-            <CompetitionCard/>
-          </div>
-        </div>
-
-        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
-          <div className='w-full rounded-lg overflow-hidden'>
-            <CompetitionCard/>
-          </div>
-        </div>
-
-        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
-          <div className='w-full rounded-lg overflow-hidden'>
-            <CompetitionCard/>
-          </div>
-        </div>
-
-        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
-          <div className='w-full rounded-lg overflow-hidden'>
-            <CompetitionCard/>
-          </div>
-        </div>
-
-        <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
-          <div className='w-full rounded-lg overflow-hidden'>
-            <CompetitionCard/>
-          </div>
-        </div>
-
+      <div className=" md:w-40 md:h-40  bg-primary overflow-hidden md:border-yellow-200 md:rounded-full"></div>
+      <div className=" md:w-40 md:h-40 bg-primary  overflow-hidden md:border-yellow-200  md:rounded-full"></div>
+      <div className=" md:w-40 md:h-40 bg-primary  overflow-hidden md:border-yellow-200  md:rounded-full"></div>
+      <div className=" md:w-40 md:h-40 bg-primary  overflow-hidden md:border-yellow-200  md:rounded-full"></div>
+      <div className=" md:w-40 md:h-40 bg-primary  overflow-hidden md:border-yellow-200  md:rounded-full"></div>
+      <div className=" md:w-40 md:h-40 bg-primary  overflow-hidden md:border-yellow-200  md:rounded-full"></div>
+      <div className=" md:w-40 md:h-40 bg-primary  overflow-hidden md:border-yellow-200  md:rounded-full"></div>
+      <div className=" md:w-40 md:h-40 bg-primary  overflow-hidden md:border-yellow-200  md:rounded-full"></div>
       </div>
-
-
+      <div className='flex flex-wrap sm:flex-row md:flex-row'>
+        {
+          compDetails.map((competition)=>(
+            <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3'>
+              <div className='w-full rounded-lg overflow-hidden' id={competition.id} >
+                <CompetitionCard title={competition.title} details={competition.details} date={competition.date}/>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </section>
   )
 }
-
 export default Competitions
