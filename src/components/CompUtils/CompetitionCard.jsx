e5f0e871db4c9526acc0e7cb31c6da1935e92b2e
Vedant Tarale <vedanttarale10@gmail.com>
2023-01-30 12:20:29 +0530

    mionr ui change

--- src/components/CompUtils/CompetitionCard.jsx
+++ src/components/CompUtils/CompetitionCard.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const CompetitionCard = (competition) => {
   return (
-    <div className='w-full h-full min-h-[28rem] max-w-sm bg-dimMaroon rounded-lg m-4 hover: shadow-md hover:shadow-rose-900'>
+    <div className="transition-all duration-100 h-fit w-fit m-0 p-1 rounded-lg bg-dimMaroon m-4 border-black border-2 hover:border-none hover:shadow-inner hover:shadow-black">
+    <div className='w-full h-full min-h-[28rem] max-w-sm bg-dimMaroon rounded-lg'>
         <div className='p-6'>
             {/* hover to some color */}
             {/* text colors and bg are subject to change */}
@@ -15,10 +16,11 @@ const CompetitionCard = (competition) => {
 
         {/* fix the hover, it should hover only for the head text and not the whole line */}
           <div className='w-full'>
-            <p className='text-sm font-poppins text-neutral-50 mt-2'>{competition.details}</p>
+            <p className='text-sm font-poppins text-neutral-50 mt-2 px-3'>{competition.details}</p>
           </div>
         </div>
     </div>  
+    </div>
   )
 }
 
