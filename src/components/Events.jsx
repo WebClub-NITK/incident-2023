8de8862bbdddaa67d4bf6c4bf1d32814ebe3dfe4
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-01-30 18:52:53 +0530

    made the basic structure

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useState } from 'react'
 
 const Events = () => {
+
+  const [currentImage, setCurrentImage] = useState(1)
+
+  const slidedata = [
+    {
+      index: 1,
+      src: "https://source.unsplash.com/Z8dtTatMVMw",
+      info: "1Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, doloremque!"
+    },
+    {
+      index: 2,
+      info: "2Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, repellendus?",
+      src: "https://images.unsplash.com/photo-1672580090136-53781593741d?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzUwMjM1ODA&ixlib=rb-4.0.3&q=80",
+    },
+    {
+      index: 3,
+      src: "https://source.unsplash.com/Z8dtTatMVMw",
+      info: "3Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, repellendus?",
+    },
+    {
+      index: 4,
+      info :"4Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, repellendus?",
+      src: "https://images.unsplash.com/photo-1672812995328-2a5459b582ad?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzUwMjM1ODA&ixlib=rb-4.0.3&q=80",
+    },
+  ]
+
+  const currentImageHandler = (index) => {
+    setCurrentImage(index)
+  }
+
+
   return (
-    <div>Events</div>
+    <div>
+      <div>Events</div>
+      <div className="flex">
+        {slidedata.map((data) => {
+          if (data.index === currentImage) {
+            return (
+              <div onClick={() => currentImageHandler(data.index)} className="w-full z-10">
+                <img src={data.src} alt='incident-events' className="w-56 h-80 object-cover mx-5 rounded-lg" />
+                <div>{data.info}</div>
+              </div>
+            )
+          } else {
+            return (
+              <div onClick={() => currentImageHandler(data.index)} className="z-0">
+                <img src={data.src} alt='incident-events' className="w-56 h-80 object-cover opacity-50 mx-5 rounded-lg" />
+                <div>{data.info}</div>
+              </div>
+            )
+          }
+
+        })
+        }
+      </div>
+    </div>
   )
 }
 
