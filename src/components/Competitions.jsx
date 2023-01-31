1a0739cf04bd7e20c5ad03df6bb12ab315feb3f2
targithu <githubtar@gmail.com>
2023-01-31 21:46:42 +0530

    dropdown

--- src/components/Competitions.jsx
+++ src/components/Competitions.jsx
@@ -13,7 +13,29 @@ const Competitions = () => {
     setCompetitions(compDetails);
     setFilterCompetitions(compDetails);
   }, []);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("Select the Category");
 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionSelection = (option) => {
+    setSelectedOption(option);
+    setTimeout(() => {
+      if(item === "All"){
+        setFilterCompetitions(competitions);
+      }
+      else{
+        setFilterCompetitions(
+          competitions.filter(
+            (competitions) => competitions.category == item
+          )
+        )
+      }
+    }, 500);
+    toggleDropdown();
+  };
   const handleCompetitionFilter = (item) => {
     setActiveFilter(item);
 
@@ -52,20 +74,15 @@ const Competitions = () => {
       </div> */}
       {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
 
-
-  <div className='md:transition-all md:duration-100 md:flex flex-wrap md:flex-row md:space-x-9 md:justify-center md:items-center flex items-center justify-center'>
+{/* For coins */}
+  <div className='hidden md:block md:transition-all md:duration-100 md:flex flex-wrap md:flex-row md:space-x-9 md:justify-center md:items-center flex items-center justify-center'>
       
         <div class="w-auto text-white font-bold py-2 px-4 border-b-4 flex flex-row justify-evenly p-1 space-x-1">
           {
             ['All','1', '2', '3', '4', '5', '6', '7'].map((item, index) => (
               
               <button 
-                key={index}
-                // onClick = {() => handleCompetitionFilter(item)}
-                // className={`text-white font-bold py-2 p-2 px-4`}
-                // className={`${activeFilter !== item? "hover:bg-primary" : " "} md:rounded-full text-white font-bold py-2 px-4 border-b-4 rounded ${
-                //   activeFilter === item ? "bg-rose-800 border-primary" : ""
-                // }`}
+                key={index} 
               >
                 
                 <img src= {logo} alt=""
@@ -82,7 +99,42 @@ const Competitions = () => {
         </div>
       
   </div>
+ {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
+{/* For dropdown */}
+<div className="block md:hidden relative ">
+      <button
+      className="w-full text-left py-2 flex items-center justify-center px-4  px-4 py-2 transform translate-y-1/2 rounded-lg text-gray-600 bg-primary border border-gray-400 focus:outline-none focus:shadow-outline hover:border-gray-500"
+        onClick={toggleDropdown}
+      >
+    
+      
+        
+        {selectedOption}
+        <svg className="ml-2 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+          <path fillRule="evenodd" d="M5.293 7. 293a1 1 0 011. 414 0L10 10.586l3. 293 -3. 293a1 1 0 111. 414 1. 414l-4 4a1 1 0 01-1. 414 0l-4-4a1 1 0 010 -1. 414z" clipRule="evenodd" />
+        </svg>
+      </button>
+      {isOpen && (
+   <div className="absolute z-10 w-56 mt-2 origin-top-right rounded-md shadow-lg">
+   <div className="bg-white rounded-md shadow-xs">
+     <div className="py-1">
+          { ['All','1', '2', '3', '4', '5', '6', '7'].map((item, index) => (
+            <a
+            key={index}
+              href="#"
+              className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+              onClick = {() => handleCompetitionFilter(item)}
+            >
+              {item}
+            </a>
+          ))}
+        </div>
+        </div></div>
+      )}
+    </div>
 
+
+  {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
 {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
 
 {/* Old competition structure for reference */}
@@ -105,7 +157,7 @@ const Competitions = () => {
         {
           filterCompetitions.map((competitions, index)=>(
             <div className='w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 flex flex-wrap justify-center'>
-              <div className='w-full rounded-lg overflow-hidden flex justify-center' id={competitions.id} >
+              <div className='w-full rounded-lg overflow-hidden flex justify-center ' id={competitions.id} >
                 <CompetitionCard 
                 key={competitions.id} 
                 index = {index}
