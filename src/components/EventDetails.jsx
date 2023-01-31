79e9c9d75efe20a92537342536f3e6da48418a61
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-01-31 21:25:16 +0530

    made changes in carousel

---
+++ src/components/EventDetails.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const EventDetails = (props) => {
+    console.log(props.data.info)
+
+    useEffect(() => {
+        const closePopUp = (e) => {
+            console.log(e.target.alt)
+            if (e.target.alt !== "incident-events") {
+                props.closeHandler(false)
+            }
+        }
+
+        document.body.addEventListener("click", closePopUp)
+
+        return () => document.body.removeEventListener("click", closePopUp)
+
+
+    })
+
+
+    return (
+        <div className="fixed top-0 left-0 w-full min-h-screen bg-slate-900/20 flex justify-center items-center backdrop-blur-md " >
+            <div className="fixed left-30% z-20 bg-slate-800 text-lg flex gap-2 rounded-xl ">
+                <div>
+                    <img src={props.data.src} className="w-56 h-80 object-cover rounded-lg " />
+                </div>
+                <div className="text-orange-600">
+                    event info ${props.data.info}
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default EventDetails;
