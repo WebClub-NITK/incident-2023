89081267204933c794a5f3c51e70f0328ba6978a
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-02-02 13:57:56 +0530

    added animation

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { GiCrossedSabres } from 'react-icons/gi';
+import "./EventDetails.css"
 
 const EventDetails = (props) => {
     console.log(props.data.info)
@@ -18,16 +20,25 @@ const EventDetails = (props) => {
 
     })
 
+    const onClickHandler = () => {
+        props.closeHandler(false)
+    }
+
 
     return (
-
-        <div id="background" className="fixed top-0 left-0 w-full min-h-screen bg-slate-900/20 flex justify-center items-center backdrop-blur-md  z-50 " >
-            <div className="fixed left-30% z-20 bg-slate-800 text-lg flex gap-2 rounded-xl ">
-                <div>
-                    <img src={props.data.src} className="w-56 h-80 object-cover rounded-lg z-100 " />
+        <div id="background" className="fixed top-0 left-0 w-full min-h-screen bg-slate-900/20 flex justify-center items-center backdrop-blur-md " >
+            <div className="fixed left-30% z-20 p-6 pt-10 bg-slate-800 text-lg flex gap-2 rounded-xl shadow-md shadow-primary">
+                <GiCrossedSabres className="text-primary absolute top-3 right-6" onClick={onClickHandler} />
+                <div className="w-56 h-80 ">
+                    <img src={props.data.src} className="w-56 h-80 object-cover rounded-lg " />
                 </div>
-                <div className="text-orange-600">
-                    event info ${props.data.info}
+                <div className="textContent">
+                    <div className="popUpText">
+                        <p>
+                            event info ${props.data.info}
+                        </p>
+                    </div>
+
                 </div>
 
             </div>
