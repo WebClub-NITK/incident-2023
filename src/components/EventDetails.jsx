2fbe1b23399bdd0c3dcbcb451912290a73cb07d7
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-02-02 20:32:09 +0530

    animated closing of popUp

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -5,11 +5,25 @@ import "./EventDetails.css"
 const EventDetails = (props) => {
     console.log(props.data.info)
 
+    const closePopUp = document.querySelector("#close-button");
+    const popUpContent = document.getElementsByClassName("textContent")
+
+    console.log(popUpContent[0])
+
     useEffect(() => {
         const closePopUp = (e) => {
-            console.log(e)
             if (e.target.id == "background") {
-                props.closeHandler(false)
+                popUpContent[0].setAttribute("closing", "");
+
+                popUpContent[0].addEventListener(
+                    "animationend",
+                    () => {
+                        popUpContent[0].removeAttribute("closing");
+                        props.closeHandler(false)
+                    },
+                    { once: true }
+                );
+
             }
         }
 
@@ -17,20 +31,29 @@ const EventDetails = (props) => {
 
         return () => document.body.removeEventListener("click", closePopUp)
 
-
     })
 
-    const onClickHandler = () => {
-        props.closeHandler(false)
+    const onClickHandler = (e) => {
+
+        popUpContent[0].setAttribute("closing", "");
+
+        popUpContent[0].addEventListener(
+            "animationend",
+            () => {
+                popUpContent[0].removeAttribute("closing");
+                props.closeHandler(false)
+            },
+            { once: true }
+        );
     }
 
 
     return (
         <div id="background" className="fixed top-0 left-0 w-full min-h-screen bg-slate-900/20 flex justify-center items-center backdrop-blur-md z-50 " >
-            <div className="fixed left-30% p-6 pt-10 bg-slate-800 text-lg flex gap-2 rounded-xl shadow-md shadow-primary ">
-                <GiCrossedSabres className="text-primary absolute top-3 right-6" onClick={onClickHandler} />
-                <div className="w-56 h-80 ">
-                    <img src={props.data.src} className="w-56 h-80 object-cover rounded-lg " />
+            <div className="fixed left-30% p-6 pt-10 bg-slate-800 text-lg flex gap-2 rounded-xl shadow-md shadow-primary " id="popUp">
+                <GiCrossedSabres className="text-primary absolute top-3 right-6" id="close-button" onClick={onClickHandler} />
+                <div>
+                    <img src={props.data.src} className="w-56 h-80 object-cover rounded-lg " id="event-image"/>
                 </div>
                 <div className="textContent">
                     <div className="popUpText">
