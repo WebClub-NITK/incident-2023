6490419611506fadab320024be066e08991a90db
Vedant Tarale <vedanttarale10@gmail.com>
2023-02-03 22:53:58 +0530

    placed sabers as modal close button and minor change to overflow scrollbar hiding code

--- src/components/CompUtils/Modal.jsx
+++ src/components/CompUtils/Modal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { close } from "../../assets";
-import { GiCrossedSabres } from 'react-icons/gi';
+import { GiCrossedSabres } from "react-icons/gi";
 import styles from "../../style";
 import { logo } from "../../assets";
 const Modal = ({ visible, details, title, date, poc, onClose }) => {
@@ -16,27 +16,19 @@ const Modal = ({ visible, details, title, date, poc, onClose }) => {
     >
       <div className="bg-gradient-to-br from-slate-900 to-black rounded max-w-[70%] max-h-[90%] rounded-3xl shadow-md shadow-secondary overflow-y-auto no-scrollbar">
         <div className="flex flex-row md-2 border-b-2 border-black">
-          <h1 className="w-full font-poppins text-4xl text-center font-medium text-primary p-2">
-            {title}
-          </h1>
-          {/* please add the following icon besides the heading so that the modal closes on clicking */}
-          <GiCrossedSabres className="text-primary absolute top-3 right-6" id="backdrop-div" onClick={handleOnClose} />
-
-          
-          {/* To be worked on */}
-          {/* <div className="inline-flex justify-end items-center basis-5/6">
-            <button className="bg-transparent lg:hover:bg-background rounded px-2 py-1">
-            <img
-            src={close}
-            alt="Close"
-            className="w-[28px] h-[28px] object-contain"
-                onClick={onClose}
-                />
-                </button>
-              </div> */}
+          <div className="basis-1/12">{/*Padding*/}</div>
+          <div className="basis-10/12">
+            <h1 className="w-full font-poppins text-4xl text-center font-medium text-primary p-2">
+              {title}
+            </h1>
+          </div>
+          <div className="flex justify-end items-center basis-1/12">
+            <button className="bg-transparent lg:hover:bg-background rounded mr-5 px-2 py-1">
+              <GiCrossedSabres className="text-primary" onClick={onClose} />
+            </button>
+          </div>
         </div>
 
-        {/* To be worked on*/}
         {/* Main Content */}
         <div className="flex h-fit items-top justify-center flex-wrap shrink-0 grow-0">
           {/* Image Div */}
