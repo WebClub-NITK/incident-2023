670a8a9234586809d3d3a8d3f5eef03128f74899
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-02-03 20:11:04 +0530

    improved some styling

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -51,9 +51,9 @@ const EventDetails = (props) => {
     return (
         <div id="background" className="fixed top-0 left-0 w-full min-h-screen bg-slate-900/20 flex justify-center items-center backdrop-blur-md z-50 " >
             <div className="fixed left-30% p-6 pt-10 bg-slate-800 text-lg flex gap-2 rounded-xl shadow-md shadow-primary " id="popUp">
-                <GiCrossedSabres className="text-primary absolute top-3 right-6" id="close-button" onClick={onClickHandler} />
-                <div>
-                    <img src={props.data.src} className="w-56 h-80 object-cover rounded-lg " id="event-image"/>
+                <GiCrossedSabres className="hover:text-primary text-secondary absolute top-3 right-6" id="close-button" onClick={onClickHandler} />
+                <div className="image-box">
+                    <img src={props.data.src} className="w-56 h-80 object-cover rounded-lg pointer-events-none " id="event-image"/>
                 </div>
                 <div className="textContent">
                     <div className="popUpText">
