86a9244f352cc34bcb3cc31e16258ddb9a3a3005
Vedant Tarale <vedanttarale10@gmail.com>
2023-02-05 16:25:57 +0530

    layout change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,27 +2,34 @@ import React from "react";
 import { logo } from "../assets";
 const About = () => {
   return (
-    <div className="bg-gradient-to-br from-slate-900 to-neutral-900 h-fit xl:rounded-xl shadow-sm">
-      <div className="text-start font-poppins text-6xl m-3 p-4 text-primary font-semibold">
-        About
+    <div className="bg-gradient-to-br from-slate-900 via-gray-900 to-neutral-900 h-fit lg:rounded-xl shadow-sm flex flex-row flex-wrap my-5">
+      <div className="basis-full md:basis-5/12">
+        <div className="text-start font-poppins text-6xl m-3 p-4 text-primary font-semibold">
+          About
+        </div>
+        <div className="text-wrap text-lg text-primary mx-3 p-4">
+          <p>
+            As the month of March approaches, every NITKian unanimously awaits
+            in eager anticipation of one event...one such extravaganza...that is
+            perhaps the highlight of their year - Incident. Incident is an
+            experience that mere words can't encapture. It is an ecstasy that
+            demands to be tasted first-hand.
+          </p>
+          <br />
+          <p>
+            Incident has been one of the flag-bearers of college fiestas in
+            India and has taken huge strides, ever since its inception in 1980,
+            to attain its current prestige.
+          </p>
+          <br />
+          <p>
+            This year marks our 43rd edition and Incident promises to be grander
+            than ever before.
+          </p>
+        </div>
       </div>
-      <div className="flex flex-row flex-wrap mx-3 px-4 pb-4">
-        <div className="basis-full md:basis-5/12 max-h-[50%] text-wrap text-lg text-primary md:bais">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, omnis
-          pariatur fugiat, error dolor perspiciatis eaque praesentium eligendi
-          voluptate deserunt neque quasi vel id facilis et placeat dolore nobis
-          a illo voluptatibus facere cupiditate ipsum. Praesentium corporis
-          voluptates fugit impedit perspiciatis nihil, aliquid, dolore commodi
-          debitis iste hic beatae libero fuga! Quaerat perferendis accusantium
-          quisquam rerum quia. Praesentium dolorum alias, placeat, nostrum ad
-          odio tempora laboriosam fuga fugiat rerum veritatis suscipit
-          recusandae commodi itaque quisquam quam vero eaque unde amet sint
-          architecto, cum porro! Similique esse tempora qui aspernatur possimus
-          officiis nulla id quam velit! Beatae eligendi delectus unde et!
-        </div>
-        <div className="basis-full md:basis-7/12 flex justify-center items-center">
-          <img src={logo} alt="" class=" max-h-[350px]" />
-        </div>
+      <div className="basis-full md:basis-7/12 flex justify-center items-center">
+        <img src={logo} alt="" class=" max-h-[350px]" />
       </div>
     </div>
   );
