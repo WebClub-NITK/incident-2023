53e3eac51e97c678ff7589b119ec2cb9fc3bab70
unknown <gandhi.mardav@gmail.com>
2023-02-05 21:14:04 +0530

    fixed commit

---
+++ src/constants/faq.js
@@ -0,0 +1,23 @@
+export const qnaSrc = [
+    {
+      id: "1",
+      question: "Who can participate?",
+      answer: "Everyone is welcome to participate, be they, developers or designers. Acceptance of applications is based on their interest in Ethereum and proven ability to BUILD - whether it’s writing software, designing beautiful UX, or something entirely else. If you are under 18 years of age, we’ll need a parental consent form."
+    },
+    {
+      id: "2",
+      question: "When will the applications close?",
+      answer: "The application period will close on 20th November 2022 at 11:59 PM IST. We evaluate applications on a rolling basis and will notify acceptance via email. Once accepted, you'll need to stake within two days to confirm your presence at the hackathon."
+    },
+    {
+      id: "3",
+      question: "How does the application process work?",
+      answer: 'Were looking for people who "do". Folks who are passionate enough to work on crazy world-changing ideas. Tell us what makes you apply to ETHIndia, and what is it that excites you or makes you proud. Or anything else that you think will strengthen your chances of acceptance. We can get to know you better from your past projects, open source contributions, GitHub/Devfolio profile, and participation in hackathons.'
+    },
+    {
+      id: "4",
+      question: "How much does it cost?",
+      answer: "Zero. Zip. Zilch. Admission to ETHIndia is entirely free, thanks to our sponsors. We'll provide you with a weekend's worth of schwag, meals, drinks, snacks, and a place to crash when you need a break from coding."
+    },
+    
+  ];
