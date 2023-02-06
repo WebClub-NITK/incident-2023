17131a783e3cc1f2ea89db530e75f44eaecc9b11
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-02-06 17:07:14 +0530

    state management

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,79 +1,59 @@
-import React from 'react'
-import {ChevronDownIcon} from '@heroicons/react/20/solid'
+import React from 'react';
+import { useState } from 'react';
+import {ChevronDownIcon} from '@heroicons/react/20/solid';
 import { qnaSrc } from '../constants/faq'
 const FAQ = () => {
 
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (j) =>{
+    if(selected==j){
+      return setSelected(null)
+    }
+    setSelected(j)
+  }
+
   return (
-    <div className="flex flex-col lg:flex-row lg:justify-between space-y-4 lg:space-x-32 px-4 md:px-12 lg:px-32 lg:pb-10">
-      <h2 className='text-8xl leading-none tracking-tight text-center md:text-left w-full xl:w-4/5 text-white'>
+
+    <div className='flex flex-wrap justify-center'>
+      <div className='w-[1240px] mx-auto grid md:grid-cols-2'>
+        <div>
+      <h2 className=' text-primary md:text-6xl sm:text-5xl text-4xl font-bold p-4'>
       Frequently asked questions
-      </h2>
-      <div className="w-full z-10">
-        <div className='text-lg xl:text-2xl'>
-
-
-
-
-
-          {/* QNA Component 1 Starts here */}
-          <button className='flex w-full justify-between py-3 mb-2 text-left border-t-2 font-circular leading-6 xl:leading-8 tracking-wider text-white' data-value='false' type = "button" aria-expanded='false'>
-            <span className='w-3/4'>First Question</span>
-          <ChevronDownIcon className={`-mr-1 ml-1 h-5 w-5`} aria-hidden='true' />
-          </button>
-          <div className='text-white flex w-full justify-between rounded-lg mb-2 text-left text-md xl:text-xl'>
-              Answer 1
-          </div>
-          {/* Upto this is one QNA component 1*/}
-
-
-
-
-
-
-          {/* QNA Component 2 Starts here */}
-          <button className='flex w-full justify-between py-3 mb-2 text-left border-t-2 font-circular leading-6 xl:leading-8 tracking-wider text-white' data-value='false' type = "button" aria-expanded='false'>
-            <span className='w-3/4'>Second Question</span>
-          <ChevronDownIcon className={`-mr-1 ml-1 h-5 w-5`} aria-hidden='true' />
-          </button>
-          <div className='text-white flex w-full justify-between rounded-lg mb-2 text-left text-md xl:text-xl'>
-              Answer 2
-          </div>
-          {/* Upto this is one QNA component 2*/}
-
-
-
-
-
-          {/* Rest all have a similar structure */}
-          {/* Task: Import the qna from the qnaSrc which i have already linked inside the constants folder of this project,  map them here instead of using these hard coded qna components */}
-
-
-
-
-
-
-          <button className='flex w-full justify-between py-3 mb-2 text-left border-t-2 font-circular leading-6 xl:leading-8 tracking-wider text-white' data-value='false' type = "button" aria-expanded='false'>
-            <span className='w-3/4'>Third Question</span>
-          <ChevronDownIcon className={`-mr-1 ml-1 h-5 w-5`} aria-hidden='true' />
-          </button>
-
-          <div className='text-white flex w-full justify-between rounded-lg mb-2 text-left text-md xl:text-xl'>
-              Answer 3
-          </div>
-
-          <button className='flex w-full justify-between py-3 mb-2 text-left border-t-2 font-circular leading-6 xl:leading-8 tracking-wider text-white' data-value='false' type = "button" aria-expanded='false'>
-            <span className='w-3/4'>Fourth Question</span>
-          <ChevronDownIcon className={`-mr-1 ml-1 h-5 w-5`} aria-hidden='true' />
-          </button>
-
-          <div className='text-white flex w-full justify-between rounded-lg mb-2 text-left text-md xl:text-xl'>
-              Answer 4
-          </div>
-
-        </div>
-      </div>
+    </h2>
     </div>
 
+      <div className='w-full flex flex-col md:mt-16 mt-2'>
+
+        {qnaSrc.map((item,i) => (
+          <div className='m-2 p-2 px-4 rounded-md w-full'>
+            <div> 
+            <button className='flex w-full justify-between py-3 mb-2 text-left border-t-2 font-bold leading-6 xl:leading-8 tracking-wider text-primary'type = "button" onClick={() => toggle(i)}>
+             <span className='w-3/4 text-xl'>{item.question}</span>
+
+
+             <span>{selected!= i?  (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" clipRule="evenodd" />
+</svg>
+): (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+<path fillRule="evenodd" d="M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z" clipRule="evenodd" />
+</svg>
+)}</span>
+           
+           </button>
+            </div>
+            <div>{selected== i? 
+            (<div className='text-secondary py-1 '> {item.answer}</div>):
+            (<div className=' py-1 hidden'> {item.answer}</div>)
+            }
+            </div>
+            </div>
+        ))}
+
+      </div>
+      </div>
+
+    </div>
   )
 }
 
