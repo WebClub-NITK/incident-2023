6bcc9247c9605ecb9a2b0e92d0192bec5ec2e583
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-02-08 00:43:50 +0530

    added image file

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {shield} from '../assets';
 
 const Sponsors = () => {
   return (
     <div>
-      <div className=' grid-cols-4 '></div>
+      <div className=' grid grid-cols-4 '>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      <img src={shield} alt="sponsor" className=" w-80"/>
+      </div>
+     
     </div>
     
   )
