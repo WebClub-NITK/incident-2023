85e79cd3895d8e808322a36a31514fa8adf795da
targithu <githubtar@gmail.com>
2023-02-14 21:16:19 +0530

    added indicators

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import {ChevronLeft,ChevronRight} from 'react-feather';
 import slides from '../constants/carousel';
+import '../constants/carousel.css';
 export default function Carousel() {
 const [current,setCurrent]=useState(0);
 const length=slides.length;
@@ -13,24 +14,39 @@ const prevSlide=()=>{
 if(!Array.isArray(slides)||slides.length<=0){
     return null;
 }
+React.useEffect(() => {
+  setTimeout(
+    () =>
+      nextSlide(
+      ),
+    3000
+  );
+
+  return () => {};
+}, [current]);
 
 return (
 <section id="carousel">
         <div className='overflow-hidden relative'>
-          <div className='flex ease-in' >
-            <div className='rounded-lg overflow-hidden'>
-            {<img src={slides[current].src} className="opacity-0.3 object-contain flex justify-center" />}
+          <div className='flex ease-in p-5' >
+            <div className='rounded-lg overflow-hidden slideshowSlider'>
+            {<img src={slides[current].src} className="object-contain flex justify-center" />}
             </div>
             <div className='w-full h-full absolute top-0 left-0 z-0'>
-                <button onClick={prevSlide} className='p-1  text-yellow-600 absolute top-1/2 left-0 z-10 rounded-full'>
+                <button onClick={prevSlide} className='p-2  text-yellow-600 absolute top-1/2 left-0 z-10 rounded-full'>
             <ChevronLeft size={40}/>
             </button>
-            <button onClick={nextSlide} className='p-1  text-yellow-600 p-1  absolute top-1/2 right-0 z-10 rounded-full'>
+            <button onClick={nextSlide} className='p-2  text-yellow-600 p-1  absolute top-1/2 right-0 z-10 rounded-full'>
             <ChevronRight size={40}/>
             </button>
             </div>        
           </div>
         </div>
+        <div className="slideshowDots">
+        {slides.map((_, idx) => (
+          <div key={idx} className={`slideshowDot${current === idx ? " active" : ""}`}></div>
+        ))}
+      </div>
 </section>
 );
 }
--- src/components/Icare.jsx
+++ src/components/Icare.jsx
@@ -3,22 +3,18 @@ import Carousel from './Carousel'
 const Icare = () => {
   return (
     <section id="icare">
-    <div className="bg-gradient-to-br from-zinc-900 via-gray-900 to-neutral-900 h-fit lg:rounded-xl shadow-sm flex flex-row flex-wrap my-5">
+    <div className=" h-fit lg:rounded-xl shadow-sm flex flex-row flex-wrap my-5 py-3">
     {/* I-care Content */}
-      <div className="sm:basis-full flex item-center justify-center text-center  underline  font-[ARMRegular] text-6xl m-3 p-4 text-primary font-semibold">
+      <div className="sm:basis-full flex item-center justify-center text-center font-[ARMRegular] text-6xl m-3 p-4 text-primary font-semibold">
       I-CARE
       </div>
       <div className="basis-full my-2 md:basis-1/2 font-poppins text-primary sm:text-1xl mx-3 p-4  my-2">
         <p>
-        We at Incident believe that conducting a large-scale cultural fest is not only about having fun, but also about
-        giving back to the community.
-        i-CARE, the social initiative wing of Incident, focuses on programmes in social-awareness, health,
-        rural-development, education, child-care and gender-equality. 
+        At Incident, a large-scale cultural fest is not only about fun, but also giving back to the community. i-CARE, the social wing of Incident, focuses on programmes such as social-awareness, health, rural-development, education, child-care and gender-equality. 
         </p>
         <br />
         <p>
-        Beach Schooling is one such initiative where underprivileged students in and around NITK are provided with
-        regular tutoring in the evenings, by the temple overlooking NITK Beach. 
+        Through Beach Schooling, underprivileged students near NITK are tutored in the evenings.
         </p>
         <br />
         <p>
