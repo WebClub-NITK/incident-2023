a9bc92ed760e984d99f12cacf1ee7414efdde9c1
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-02-14 23:57:26 +0530

    added for xl lg classes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,8 @@ const Hero = () => {
         className="sm:w-[1910px]  w-[1400px] "
       >  
       
-      <img className="absolute md:top-[662px] sm:top-[800px] top-[662px] xl:left-60 md:left-40 sm:left-10 left-4 sm:w-[210px] md:w-[315px] w-[140px] mx-auto " src={bard} alt="/"/>
-      <img className="absolute md:top-[640px] sm:top-[800px] top-[660px] xl:right-60 md:right-40 sm:right-10 right-3 sm:w-[260px] md:w-[420px] w-[180px] mx-auto " src={knight} alt="/"/>
+      <img className="absolute md:top-[662px] sm:top-[800px] top-[662px] xl:left-60 lg:left-14 md:left-12 sm:left-10 left-4 sm:w-[210px] md:w-[315px] w-[140px] mx-auto " src={bard} alt="/"/>
+      <img className="absolute md:top-[640px] sm:top-[800px] top-[660px] xl:right-60 lg:right-14 md:right-12 sm:right-10 right-3 sm:w-[260px] md:w-[420px] w-[180px] mx-auto " src={knight} alt="/"/>
       
       </Player>
 
@@ -35,8 +35,8 @@ const Hero = () => {
       
       <div className='text-white ">'>
         <div className='mx-auto flex flex-col justify-center'>
-          <h1 className='text-gradient font-[RMMedium] md:text-8xl sm:text-7xl text-5xl font-bold md:py-6 mx-auto '>Incident '23</h1>
-          <p className='md:text-7xl sm:text-6xl text-4xl mt-2 font-[ELand] mx-auto'>A tale of Bards & Knights</p>
+          <h1 className='text-gradient font-[RMMedium] xl:text-8xl md:text-7xl sm:text-7xl text-5xl font-bold md:py-6 mx-auto '>Incident '23</h1>
+          <p className='xl:text-7xl md:text-6xl sm:text-6xl text-4xl mt-2 font-[ELand] mx-auto'>A tale of Bards & Knights</p>
           <img className='w-[320px] mx-auto mt-[-40px]' src={art}/>
           <h1 className='text-primary text-2xl font-bold mx-auto mt-[-45px]'>16 March to 19 March</h1>
           
