0a68dd4bfddf7663260c8f7f24a172b831908baf
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-02-19 14:03:27 +0530

    Fixed fonts size

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -44,7 +44,7 @@ const FAQ = () => {
           <div className='m-1 p-1 px-2 rounded-md w-full'>
             <div> 
             <button className='font-[ARMBook] flex w-full justify-between py-3 mb-1 text-left border-t-2 font-bold leading-6 xl:leading-8 tracking-wider text-primary'type = "button" onClick={() => toggle(i)}>
-             <span className=' md:text-xl sm:text-xl text-md'>{item.question}</span>
+             <span className='md:text-xl sm:text-xl text-md'>{item.question}</span>
 
              <span>{selected!= i?  (
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
@@ -62,7 +62,7 @@ const FAQ = () => {
            </button>
 
             </div>
-                <div>{selected== i ? (<div className='text-secondary py-1 font-[poppins] sm:text-xl text-base'> {item.answer}</div>) : (<div className=' py-1 hidden  '> {item.answer}</div>)}
+                <div>{selected== i ? (<div className='text-secondary py-1 font-[poppins] sm:text-xl text-md'> {item.answer}</div>) : (<div className=' py-1 hidden  '> {item.answer}</div>)}
             </div>
             </div>
         ))}
