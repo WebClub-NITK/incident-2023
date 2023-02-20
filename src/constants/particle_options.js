526c502c92a74bdb194fd8a32f33b476ce4b07a3
Vedant Tarale <vedanttarale10@gmail.com>
2023-02-20 09:43:20 +0530

    fixed interactivity

--- src/constants/particle_options.js
+++ src/constants/particle_options.js
@@ -29,21 +29,5 @@ const data = {
       attract: { enable: false, rotateX: 600, rotateY: 1200 },
     },
   },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: { enable: false, mode: "bubble" },
-      onclick: { enable: false, mode: "repulse" },
-      resize: true,
-    },
-    modes: {
-      grab: { distance: 400, line_linked: { opacity: 0.5 } },
-      bubble: { distance: 400, size: 4, duration: 0.3, opacity: 1, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 },
-    },
-  },
-  retina_detect: true,
 };
 export default data;
