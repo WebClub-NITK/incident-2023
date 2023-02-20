fa4540fa9718b0fe24debd839c2e587319bb9955
VedantTarale <95867745+VedantTarale@users.noreply.github.com>
2023-02-20 10:39:54 +0530

    Fixes Interactivity (#31)
    * fixed interactivity
    
    * fixed links and hover effect
    
    * changed contact details

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,28 +33,28 @@ export const socialMedia = [
   {
     id: "social-media-1",
     icon: AiFillLinkedin,
-    link: "Incident-Linkedin",
+    link: "https://www.linkedin.com/company/incident-nitk2020/",
   },
   {
     id: "social-media-3",
     icon: AiFillMail,
-    link: "Incident-Mail",
+    link: "mailto:incident@nitk.edu.in",
   },
   {
     id: "social-media-4",
     icon: AiOutlineTwitter,
-    link: "Incident-Twitter",
+    link: "https://twitter.com/incident_nitk",
   },
   {
     id: "social-media-5",
     icon: AiFillInstagram,
-    link: "Incident-Insta",
+    link: "https://www.instagram.com/incident_nitk/",
   },
 ];
 
 export const aboutMe = {
   name: "Incident 2023",
-  tagLineOne: "Contact: +91 8884147838",
+  tagLineOne: "Contact: +91 9036973038",
   tagLineTwo: "Email: incident@nitk.edu.in",
   intro: "South India's largest cultural fest?"
 }
