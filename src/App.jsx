fb9cdb23412303d887bff9fa6e0154d3a176689b
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-02-23 23:01:16 +0530

    added hyperlink

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ const App = () => {
         </div>
         <Footer />
         <div className="bg-background w-full text-secondary text-lg flex flex-nowrap p-2  justify-center gap-2 items-center">
-          Made with <img src={heart} className="w-5"/> by WEC-NITK
+          Made with <img src={heart} className="w-5"/> by <a href="https://webclub.nitk.ac.in/" target="_blank" className="no-underline hover:text-primary">WEC-NITK</a>
         </div>
       </div>
     )
