2c1aaa5a8b38418c20ee579afd4cf27a99676f7e
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-03-10 19:10:05 +0530

    modified modal and added events details

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { GiCrossedSabres } from 'react-icons/gi';
 import "./EventDetails.css"
+import ReactMarkdown from 'react-markdown'
 
 const EventDetails = (props) => {
     console.log(props.data.info)
@@ -50,23 +51,29 @@ const EventDetails = (props) => {
 
     return (
         <div id="background" className="fixed top-0 left-0 w-full min-h-screen bg-slate-900/20 flex justify-center items-center backdrop-blur-md z-50 " >
-            <div className="fixed left-30% p-6 pt-10 bg-slate-800 text-lg flex gap-2 rounded-xl shadow-md shadow-primary " id="popUp">
+            <div className="fixed left-30% p-6 pt-8 bg-slate-800 text-lg flex gap-10 rounded-xl shadow-md shadow-primary " id="popUp">
                 <GiCrossedSabres className="hover:text-primary text-secondary absolute top-3 right-6" id="close-button" onClick={onClickHandler} />
-                <div className="image-box w-64 ">
-                    <img src={props.data.src} className="w-full h-full object-cover rounded-lg pointer-events-none " id="event-image"/>
+                <div className="image-box w-72 ">
+                    <img src={props.data.src} className="w-full h-full object-cover rounded-lg pointer-events-none " id="event-image" />
                 </div>
                 <div className="textContent">
                     <div className="popUpText">
-                        <p>
-                            event info ${props.data.info}
+                        <ReactMarkdown className="font-poppins text-4xl text-center font-medium text-primary py-3 border-b-2 border-black">
+                            {props.data.heading}
+                        </ReactMarkdown>
+                        <p className="text-primary text-2xl font-semibold">
+                            Date: {props.data.date}
                         </p>
+                        <ReactMarkdown className=" text-primary text-left text-lg">
+                            {props.data.info}
+                        </ReactMarkdown>
                     </div>
 
                 </div>
 
             </div>
         </div>
-
+        
     )
 }
 
